ffdc319718a9a53146542d8ccc548cfa86fb815b
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-22 18:19:00 +0500

    make changes

--- components/header.js
+++ components/header.js
@@ -1,8 +1,12 @@
-const Header = ({ hamburger }) => {
+import Link from "next/link";
+const Header = () => {
   return (
     <header className="self-stretch bg-gray-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] [background:white] z-[2] text-center text-5xl text-primary-500 font-body-regular-600 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
       <div className="flex-1 flex flex-row items-center justify-between">
-        <div className="flex flex-row items-center justify-center gap-[8px]">
+        <Link href="/"
+          className="flex flex-row items-center justify-center
+          gap-[8px]"
+        >
           <img className="relative w-11 h-11" alt="" src="/houseline.svg" />
           <div className="flex flex-col items-start justify-start">
             <div className="relative leading-[24px] font-semibold">REIS</div>
@@ -10,7 +14,8 @@ const Header = ({ hamburger }) => {
               Real State
             </div>
           </div>
-        </div>
+        </Link>
+        
         <div className="flex flex-row items-center justify-end gap-[36px] text-sm text-primary-900 sm:flex">
           <div className="flex flex-row items-start justify-start gap-[30px] lg:hidden">
             <div className="relative leading-[22px]">HOME</div>
@@ -24,15 +29,7 @@ const Header = ({ hamburger }) => {
             <div className="relative leading-[22px]">CONTACT US</div>
             <div className="relative leading-[22px]">SEARCH</div>
           </div>
-          {!hamburger && (
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] hidden flex-row items-center justify-center lg:flex">
-              <img
-                className="relative w-6 h-6 overflow-hidden shrink-0 md:flex"
-                alt=""
-                src="/notification.svg"
-              />
-            </button>
-          )}
+          
         </div>
       </div>
     </header>
--- components/rent-properties-container.js
+++ components/rent-properties-container.js
@@ -1,6 +1,6 @@
 import PropertyCard from "./property-card";
 
-const RentPropertiesContainer = () => {
+const RentPropertiesContainer = ({}) => {
   return (
     <div className="self-stretch flex flex-col py-[86px] px-0 items-center justify-start gap-[39px] text-center text-21xl text-primary-800 font-body-regular-600">
       <div className="self-stretch flex flex-col items-center justify-start gap-[40px] max-w-[95%px] md:pl-[60px] md:pr-[60px] md:box-border">
@@ -16,60 +16,24 @@ const RentPropertiesContainer = () => {
         </div>
         <div className="self-stretch flex flex-row flex-wrap py-0 px-2.5 items-center justify-center gap-[32px]">
           <PropertyCard
-            propertyImage="/unsplashrlwe8f8anoc7@2x.png"
-            car="/car5.svg"
-            bathtub="/bathtub5.svg"
-            arrowsOut="/arrowsout2.svg"
-            ellipse1="/ellipse-1@2x.png"
-            shareNetwork="/sharenetwork6.svg"
-            heart="/heart2.svg"
-            plus="/plus7.svg"
-            propWidth="unset"
-            propFlex="1"
-            propMinWidth="355px"
-            propMaxWidth="400px"
+            imgUrl="/unsplashrlwe8f8anoc7@2x.png"
+            name="92 ALLIUM PLACE, ORLANDO FL 32827"
+            price="590,632"
           />
           <PropertyCard
-            propertyImage="/unsplashrlwe8f8anoc8@2x.png"
-            car="/car5.svg"
-            bathtub="/bathtub5.svg"
-            arrowsOut="/arrowsout2.svg"
-            ellipse1="/ellipse-1@2x.png"
-            shareNetwork="/sharenetwork6.svg"
-            heart="/heart2.svg"
-            plus="/plus8.svg"
-            propWidth="unset"
-            propFlex="1"
-            propMinWidth="355px"
-            propMaxWidth="400px"
+            imgUrl="/unsplashrlwe8f8anoc8@2x.png"
+            name="92 ALLIUM PLACE, ORLANDO FL 32827"
+            price="590,632"
           />
           <PropertyCard
-            propertyImage="/unsplashrlwe8f8anoc9@2x.png"
-            car="/car6.svg"
-            bathtub="/bathtub.svg"
-            arrowsOut="/arrowsout3.svg"
-            ellipse1="/ellipse-1@2x.png"
-            shareNetwork="/sharenetwork7.svg"
-            heart="/heart2.svg"
-            plus="/plus3.svg"
-            propWidth="unset"
-            propFlex="1"
-            propMinWidth="355px"
-            propMaxWidth="400px"
+            imgUrl="/unsplashrlwe8f8anoc9@2x.png"
+            name="92 ALLIUM PLACE, ORLANDO FL 32827"
+            price="590,632"
           />
           <PropertyCard
-            propertyImage="/unsplashrlwe8f8anoc10@2x.png"
-            car="/car6.svg"
-            bathtub="/bathtub.svg"
-            arrowsOut="/arrowsout3.svg"
-            ellipse1="/ellipse-1@2x.png"
-            shareNetwork="/sharenetwork7.svg"
-            heart="/heart2.svg"
-            plus="/plus3.svg"
-            propWidth="unset"
-            propFlex="1"
-            propMinWidth="355px"
-            propMaxWidth="400px"
+            imgUrl="/unsplashrlwe8f8anoc10@2x.png"
+            name="92 ALLIUM PLACE, ORLANDO FL 32827"
+            price="590,632"
           />
         </div>
       </div>
