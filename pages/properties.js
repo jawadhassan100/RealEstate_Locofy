546e090b60b290f01f3a9506434329a2b293b8a7
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-26 18:09:50 +0500

    responsiveness update

--- pages/properties.js
+++ pages/properties.js
@@ -1,30 +1,26 @@
 import "antd/dist/antd.min.css";
 import { Pagination, Dropdown } from "antd";
 import { DownOutlined } from "@ant-design/icons";
-import {createClient} from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 import Header from "../components/header";
 import PropertiesGridContainer from "../components/properties-grid-container";
 import Footer from "../components/footer";
 import { useEffect, useState } from "react";
 
-
-
 const defaultOrder = [
   {
     key: "1",
-    label:( <a onClick={(e) => e.preventDefault()}>Popular Properties</a>)
+    label: <a onClick={(e) => e.preventDefault()}>Popular Properties</a>,
   },
   {
     key: "2",
-    label: (<a onClick={(e) => e.preventDefault()}>Latest Properties</a>)
+    label: <a onClick={(e) => e.preventDefault()}>Latest Properties</a>,
   },
   {
     key: "3",
-    label: (<a onClick={(e) => e.preventDefault()}>Recommended Properties</a>)
+    label: <a onClick={(e) => e.preventDefault()}>Recommended Properties</a>,
   },
-  
 ];
-
 
 const PropertiesGridView = () => {
   const client = createClient(
@@ -32,15 +28,15 @@ const PropertiesGridView = () => {
     process.env.NEXT_PUBLIC_KEY
   );
 
-  const [properties , setProperties] = useState([])
+  const [properties, setProperties] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchProperties = async () => {
-      const result = await client.from("properties").select("*")
+      const result = await client.from("properties").select("*");
       setProperties(result.data);
-    } 
-     fetchProperties()
-  },[])
+    };
+    fetchProperties();
+  }, []);
 
   return (
     <div className=" bg-gray-white w-full flex flex-col items-start justify-start text-center text-33xl text-gray-white font-body-regular-400">
@@ -54,16 +50,16 @@ const PropertiesGridView = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col pt-16  pb-2 items-center justify-start gap-[95px] text-left text-base text-gray-black font-body-regular-600 lg:pl-[120px] lg:pr-[120px] lg:box-border ">
+      <div className="flex flex-col pt-16  pb-2 items-center justify-start gap-[95px] text-left text-base text-gray-black font-body-regular-600 lg:pl-[120px] lg:pr-[120px] lg:box-border md:pl[60px] md:pr-[60px] md:box-border sm:pl-5 sm:pr-5 sm:box-border">
         <div className=" flex flex-row items-center justify-start">
-          <div className="flex flex-row flex-wrap items-end justify-start gap-[16px]">
+          <div className="flex flex-row  items-end justify-start gap-[16px]">
             <div className="flex flex-row items-start justify-start gap-[8px]">
               <img className=" w-6 h-6" alt="" src="/listbullets.svg" />
               <img className=" w-6 h-6" alt="" src="/squaresfour.svg" />
             </div>
             <div className=" leading-[24px]">Sort by:</div>
             <Dropdown
-              menu={{ items : defaultOrder}}
+              menu={{ items: defaultOrder }}
               placement="bottomLeft"
               trigger={["hover"]}
             >
@@ -74,9 +70,7 @@ const PropertiesGridView = () => {
             </Dropdown>
           </div>
         </div>
-        <PropertiesGridContainer 
-         allProperties={properties} 
-        />
+        <PropertiesGridContainer allProperties={properties} />
         <div className="flex flex-row items-end justify-center gap-[8px] text-center text-primary-500">
           <Pagination defaultCurrent={1} total={50} />
         </div>
